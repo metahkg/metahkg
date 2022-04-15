a163a39d4a87ceb4ce17a88b1bcbe1de5ba49808
wcyat <sleep@wcyat.me>
2022-04-15 13:52:40 +0800

    update mongodb

---
+++ mongodb/mongo-setup.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const { exit } = require("process");
+const fs = require("fs");
+const mongouri = process.env.DB_URI || "mongodb://localhost"; //mongo connection string
+async function setup() {
+  const client = new MongoClient(mongouri);
+  await client.connect();
+  const db = client.db("metahkg");
+  if ((await db.collection("category").find().toArray()).length) {
+    console.log("documents found. not inserting categories.");
+  } else {
+    await db
+      .collection("category")
+      .insertMany(JSON.parse(fs.readSync("category.json")));
+  }
+  await db
+    .collection("viral")
+    .createIndex({ createdAt: 1 }, { expireAfterSeconds: 172800 });
+  await db.collection("summary").createIndex({ op: "text", title: "text" }); //text search
+  await db
+    .collection("limit")
+    .createIndex({ createdAt: 1 }, { expireAfterSeconds: 86400 });
+  await db
+    .collection("verification")
+    .createIndex({ createdAt: 1 }, { expireAfterSeconds: 300 });
+  exit(0);
+}
+setup();
