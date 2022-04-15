a9ab3cc1650d16a01ed92e7dbf1dcc9134328e02
wcyat <sleep@wcyat.me>
2022-04-15 14:05:21 +0800

    update mongo setup

--- mongodb/mongo-setup.js
+++ mongodb/mongo-setup.js
@@ -2,28 +2,25 @@ require("dotenv").config();
 const { MongoClient } = require("mongodb");
 const { exit } = require("process");
 const fs = require("fs");
-const mongouri = process.env.DB_URI || "mongodb://localhost:30000"; //mongo connection string
+const mongouri = process.env.DB_URI || "mongodb://username:password@localhost:30000"; //mongo connection string
 async function setup() {
-  const client = new MongoClient(mongouri);
-  await client.connect();
-  const db = client.db("metahkg");
-  if ((await db.collection("category").find().toArray()).length) {
-    console.log("documents found. not inserting categories.");
-  } else {
-    await db
-      .collection("category")
-      .insertMany(JSON.parse(fs.readSync("category.json")));
-  }
-  await db
-    .collection("viral")
-    .createIndex({ createdAt: 1 }, { expireAfterSeconds: 172800 });
-  await db.collection("summary").createIndex({ op: "text", title: "text" }); //text search
-  await db
-    .collection("limit")
-    .createIndex({ createdAt: 1 }, { expireAfterSeconds: 86400 });
-  await db
-    .collection("verification")
-    .createIndex({ createdAt: 1 }, { expireAfterSeconds: 300 });
-  exit(0);
+    const client = new MongoClient(mongouri);
+    await client.connect();
+    const db = client.db("metahkg");
+    const categoryCl = db.collection("category");
+    const viralCl = db.collection("viral");
+    const summaryCl = db.collection("summary");
+    const limitCl = db.collection("limit");
+    const verificationCl = db.collection("verification");
+    if ((await categoryCl.find().toArray()).length) {
+        console.log("documents found. not setting up again");
+    } else {
+        await categoryCl.insertMany(JSON.parse(fs.readSync("category.json")));
+        await viralCl.createIndex({ createdAt: 1 }, { expireAfterSeconds: 172800 });
+        await summaryCl.createIndex({ op: "text", title: "text" }); //text search
+        await limitCl.createIndex({ createdAt: 1 }, { expireAfterSeconds: 86400 });
+        await verificationCl.createIndex({ createdAt: 1 }, { expireAfterSeconds: 300 });
+    }
+    exit(0);
 }
 setup();
