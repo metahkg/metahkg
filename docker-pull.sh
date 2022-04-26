f4241614b25e644cae06a6b624e5daa2fd2b98e2
wcyat <wcyat@wcyat.me>
2022-04-27 00:35:10 +0800

    experimental prebuilt images

---
+++ docker-pull.sh
@@ -0,0 +1,6 @@
+docker pull registry.gitlab.io/metahkg/metahkg-web/master:latest && \
+docker pull registry.gitlab.io/metahkg/metahkg-web/dev:latest && \
+docker pull registry.gitlab.io/metahkg/metahkg-server/master:latest && \
+docker pull registry.gitlab.io/metahkg/metahkg-server/dev:latest && \
+docker pull registry.gitlab.io/metahkg/metahkg-links/master:latest && \
+docker pull registry.gitlab.io/metahkg/metahkg-links/dev:latest
