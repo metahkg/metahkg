dd4123a3715bb0cbcfc839f75d1da3304bb348df
wcyat <wcyat@wcyat.me>
2022-04-27 13:01:40 +0800

    support prebuilt images!

--- docker-pull.sh
+++ docker-pull.sh
@@ -1,6 +1,6 @@
-docker pull registry.gitlab.io/metahkg/metahkg-web/master:latest && \
-docker pull registry.gitlab.io/metahkg/metahkg-web/dev:latest && \
-docker pull registry.gitlab.io/metahkg/metahkg-server/master:latest && \
-docker pull registry.gitlab.io/metahkg/metahkg-server/dev:latest && \
-docker pull registry.gitlab.io/metahkg/metahkg-links/master:latest && \
-docker pull registry.gitlab.io/metahkg/metahkg-links/dev:latest
+docker pull registry.gitlab.com/metahkg/metahkg-web/master:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-web/dev:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-server/master:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-server/dev:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-links/master:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-links/dev:latest
