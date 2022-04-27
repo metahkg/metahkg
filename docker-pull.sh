062d83b4259d9ff09c66c4bcc64f508a2a6bec73
wcyat <wcyat@wcyat.me>
2022-04-27 13:08:08 +0800

    merge dev

---
+++ docker-pull.sh
@@ -0,0 +1,6 @@
+docker pull registry.gitlab.com/metahkg/metahkg-web/master:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-web/dev:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-server/master:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-server/dev:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-links/master:latest && \
+docker pull registry.gitlab.com/metahkg/metahkg-links/dev:latest
