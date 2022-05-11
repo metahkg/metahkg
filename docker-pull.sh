223cc5a678d548715d8d03b1d392eb392fddfba0
wcyat <wcyat@wcyat.me>
2022-05-11 21:54:15 +0800

    pull merged in command

--- docker-pull.sh
+++
@@ -1,6 +0,0 @@
-docker pull registry.gitlab.com/metahkg/metahkg-web/master:latest && \
-docker pull registry.gitlab.com/metahkg/metahkg-web/dev:latest && \
-docker pull registry.gitlab.com/metahkg/metahkg-server/master:latest && \
-docker pull registry.gitlab.com/metahkg/metahkg-server/dev:latest && \
-docker pull registry.gitlab.com/metahkg/metahkg-links/master:latest && \
-docker pull registry.gitlab.com/metahkg/metahkg-links/dev:latest
