97cda9b3df98e01dcb5a6e2f69147ed2ee71ae15
wcyat <wcyat@wcyat.me>
2022-11-12 22:43:56 +0800

    mongodb: add rlp to mongo-init.sh

--- mongodb/mongo-init.sh
+++ mongodb/mongo-init.sh
@@ -16,4 +16,12 @@ db.createUser({
   pwd: '$MONGO_INITDB_ROOT_PASSWORD',
   roles: [{ role: 'readWrite', db: 'agenda' }],
 });
+
+db = db.getSiblingDB('rlp')
+
+db.createUser({
+  user: '$MONGO_INITDB_ROOT_USER',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+  roles: [{ role: 'readWrite', db: 'rlp' }],
+});
 EOF
