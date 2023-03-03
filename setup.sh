2529f2e69f22189a95632bca10b4a11d548c94bf
wcyat <wcyat@wcyat.me>
2023-03-04 00:20:28 +0800

    fix(setup.sh): double quotes

--- setup.sh
+++ setup.sh
@@ -254,7 +254,7 @@ check_arch () {
             ARCH='arm64'
         ;;
         *)
-            echo "Error: Unsupported architecture "$ARCH""
+            echo "Error: Unsupported architecture $ARCH"
             echo "Use --no-check-arch to bypass this check"
             exit 1
         ;;
@@ -366,7 +366,7 @@ config_env() {
     echo ""
 
     input -p "Do you want to use Mongo Express (a mongodb gui, https://github.com/mongo-express/mongo-express)?" -o "y, n" -d "n" MONGO_EXPRESS;
-    if [ ""$MONGO_EXPRESS"" = "y" ]; then
+    if [ "$MONGO_EXPRESS" = "y" ]; then
         input -p "Port for mongo express" -d "$MONGO_EXPRESS_PORT" MONGO_EXPRESS_PORT;
     fi;
 
@@ -392,7 +392,7 @@ config_env() {
     echo "An example using gmail: https://forwardemail.net/en/guides/send-mail-as-gmail-custom-domain.";
     input -p "Your choice" -o "mailgun, smtp" -d mailgun EMAIL_PROVIDER;
 
-    case ""$EMAIL_PROVIDER"" in
+    case "$EMAIL_PROVIDER" in
         mailgun)
             input -p "Mailgun api key (obtain one at https://mailgun.com)" -d "$MAILGUN_KEY" MAILGUN_KEY;
             input -p "Mailgun domain" -d "${MAILGUN_DOMAIN:-$DOMAIN}" MAILGUN_DOMAIN;
@@ -603,16 +603,16 @@ echo ""
 echo "Metahkg is now configured."
 echo "You can start metahkg by running:"
 CMD="yarn docker"
-if [ ""$PROTONVPN"" = "y" ]; then
+if [ "$PROTONVPN" = "y" ]; then
     CMD+=":vpn"
 fi;
-if [ ""$MONGO_EXPRESS"" = "y" ]; then
+if [ "$MONGO_EXPRESS" = "y" ]; then
     CMD+=":express"
 fi;
-if [ ""$PREBUILT"" = "y" ]; then
+if [ "$PREBUILT" = "y" ]; then
     CMD+=":prebuilt"
 fi;
-echo ""$CMD""
+echo "$CMD"
 echo "Then, you can access metahkg at http://localhost:${PORT}"
 echo ""
 
